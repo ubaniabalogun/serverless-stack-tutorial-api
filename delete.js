a317eeb66a2f547b1d0fceed16d7bb870bcb1796
Ubani Balogun <balogunubani@gmail.com>
2021-01-11 21:49:07 -0800

    Completed Secure The APIs

--- delete.js
+++ delete.js
@@ -5,7 +5,7 @@ export const main = handler(async (event, context) => {
   const params = {
     TableName: process.env.tableName,
     Key: {
-      userId: "123",
+      userId: event.requestContext.identity.cognitoIdentityId, // The id of the author
       noteId: event.pathParameters.id,
     },
   };
